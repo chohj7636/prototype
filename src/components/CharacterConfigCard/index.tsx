b5db2b6d86857e11b6f45906c4e0f79893a6390b
owen <chohj7636@gmail.com>
2025-03-12 16:54:26 +0900

    특정 key를 가진 캐릭터설정 카드에 value 넣기

--- src/components/CharacterConfigCard/index.tsx
+++ src/components/CharacterConfigCard/index.tsx
@@ -1,6 +1,13 @@
+import useMakeScenarioStore from '@/hooks/useMakeScenario';
 import { Input } from '../ui/input';
 
-const CharacterConfigCard = () => {
+interface CharacterConfigCardProps {
+  index: number;
+}
+
+const CharacterConfigCard = ({ index }: CharacterConfigCardProps) => {
+  const { characterConfig, setCharacterConfig } = useMakeScenarioStore();
+
   return (
     <div className="flex w-full max-w-[640px] flex-col gap-2 rounded-[10px] border border-[#d9d9d9] p-[10px]">
       <div className="flex w-full items-center gap-2">
@@ -8,6 +15,8 @@ const CharacterConfigCard = () => {
         <Input
           className="w-[500px]"
           placeholder="캐릭터 이름을 입력해주세요."
+          value={characterConfig[index].name || ''}
+          onChange={(e) => setCharacterConfig(index, 'name', e.target.value)}
         />
       </div>
       <div className="flex w-full items-center gap-2">
@@ -15,6 +24,8 @@ const CharacterConfigCard = () => {
         <Input
           className="w-[500px]"
           placeholder="캐릭터 외형을 입력해주세요."
+          value={characterConfig[index].style || ''}
+          onChange={(e) => setCharacterConfig(index, 'style', e.target.value)}
         />
       </div>
       <div className="flex w-full items-center gap-2">
@@ -22,6 +33,8 @@ const CharacterConfigCard = () => {
         <Input
           className="w-[500px]"
           placeholder="캐릭터 특징을 입력해주세요."
+          value={characterConfig[index].feature || ''}
+          onChange={(e) => setCharacterConfig(index, 'feature', e.target.value)}
         />
       </div>
     </div>
