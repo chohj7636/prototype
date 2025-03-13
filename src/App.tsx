88668f0170bb7c58fdbb5879039f02db605609a1
owen <chohj7636@gmail.com>
2025-03-13 13:49:08 +0900

    리액트 쿼리 install

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import CreateStoryWrapper from './components/CreateStoryWrapper';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="h-full min-h-[100vh] w-full">
-      <div className="h-[80px] w-full bg-[#efedfb]" />
-      <div
-        id="prototype-body"
-        className="flex h-full w-full flex-col items-center justify-center gap-[20px] p-5"
-      >
-        <CreateStoryWrapper />
+    <QueryClientProvider client={queryClient}>
+      <div className="h-full min-h-[100vh] w-full">
+        <div className="h-[80px] w-full bg-[#efedfb]" />
+        <div
+          id="prototype-body"
+          className="flex h-full w-full flex-col items-center justify-center gap-[20px] p-5"
+        >
+          <CreateStoryWrapper />
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
